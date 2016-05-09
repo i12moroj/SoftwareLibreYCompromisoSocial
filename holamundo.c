0e57a3b35a1e5fdb7b6cfe1bde1389dccb921990
i12moroj <i12moroj@uco.es>
2016-05-10 01:01:37 +0200

    Hola de nuevo

--- holamundo.c
+++ holamundo.c
@@ -1,8 +1,11 @@
 # include "./includes/miinclude.h"
-#include <string.h>
+# include <string.h>
+
 int main(){
 	char c[100]= "Hola mundo";
+	char d[100]= "Hola";
 	int x;
 	printf("%s\n", strcat(c,", soy de nueva carteya"));
+	printf("%s\n", strcat(d,", y yo soy de Córdoba :)"));
 	return 0;
 }
